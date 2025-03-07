5f53939456df2af0367e552d114648d9d754a9d5
Bertrand Shao <bertrandshao@gmail.com>
2025-03-07 00:27:15 -0800

    learn about obj key sorting

--- non-react/random.js
+++ non-react/random.js
@@ -8,3 +8,35 @@ while (i > 0)
 // parts of loops can be omitted: for(;;) actually works
 
 console.log(false == 0, 'should be true because of coercion?')
+
+// transpilers convert code to older JavaScript code that older JavaScript engines can read
+// polyfills fills in when there is a lack of implementation for things like newer functions (like Math.trunc)
+
+let user = new Object()
+user["with space"] = "it worked"
+
+console.log(user, 'wow you can make an object this way {} and use space')
+
+let variable = "variable"
+
+let bag = {
+  [variable]: 5
+}
+
+console.log(bag, bag[variable])
+
+// there are no limitations on property names. you can use return, like obj.return.
+
+// integer property means a string can be turned to an integer then back, like "42". "+49" is not integer property.
+// integer property keys get sorted in an obj automatically. other keys don't.
+
+const sortedObj = {
+  3: "",
+  "2": "",
+  "don't sort": "",
+  1: ""
+}
+
+for (const key in sortedObj) {
+  console.log(key, 'key')
+}
