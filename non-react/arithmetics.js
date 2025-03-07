7c883716ef5782600a53968b33b20e5db4adde14
Bertrand Shao <bertrandshao@gmail.com>
2025-03-07 01:12:19 -0800

    add prettier

--- non-react/arithmetics.js
+++ non-react/arithmetics.js
@@ -7,21 +7,21 @@ console.log(9007199254740991 + 2); // 9007199254740992
 
 const bigInt = 1234567890123456789012345678901234567890n;
 
-console.log(bigInt, "bigInt")
+console.log(bigInt, "bigInt");
 
 // + operator with string turns everything to concatenate as a string
-console.log(1 + "2") // this is 12
+console.log(1 + "2"); // this is 12
 
 // subtraction triggers a numeric conversion
-console.log(3 - "2") // this is 1
+console.log(3 - "2"); // this is 1
 
 // + converts empty arrays and objects into "" and - converts them into the number 0
 
 // empty array and objects are truthy, but empty strings and zeros are falsy
 
 // with arithmetic, null is treated as 0, but undefined is NaN
-console.log(null + 1) // 1
-console.log(undefined + 1) // NaN
+console.log(null + 1); // 1
+console.log(undefined + 1); // NaN
 
 // when using * or /, strings get converted into numbers
 console.log("10" * 2); // 20
@@ -29,10 +29,10 @@ console.log("10" / 2); // 5
 console.log("10" * "2"); // 20
 console.log("10" / "2"); // 5
 
-console.log("" + 10) // coerced to string
-console.log("" * 10) // coerced to 0
+console.log("" + 10); // coerced to string
+console.log("" * 10); // coerced to 0
 
-console.log({} * 1) // becomes "[object Object]" <- the string representation of an Object when it is converted to a string
+console.log({} * 1); // becomes "[object Object]" <- the string representation of an Object when it is converted to a string
 
 // true gets coerced to 1, false to 0, null to 0, undefined to NaN - this is because undefined logically speaking is neither definitively truthy nor falsy
 // with arrays, single elements are coerced to their primitive value: [2] to 2
@@ -41,22 +41,22 @@ console.log([2, 3] + 2); // 2,32
 console.log([2, 3] + ""); // 2,3, wow, this is neat
 
 console.log(Number("   123   ")); // 123
-console.log(Number("123z"));      // NaN (error reading a number at "z")
-console.log(Number(true));        // 1
-console.log(Number(false));       // 0
+console.log(Number("123z")); // NaN (error reading a number at "z")
+console.log(Number(true)); // 1
+console.log(Number(false)); // 0
 
 // don't forget the exponentiation operand exists! **
-console.log(2 ** 5) // 32
+console.log(2 ** 5); // 32
 
-console.log(2 + 2 + '1'); // "41" and not "221"
-console.log('1' + 2 + 2); // "122" and not "14"
+console.log(2 + 2 + "1"); // "41" and not "221"
+console.log("1" + 2 + 2); // "122" and not "14"
 // operation goes left to right
 
 // + is an unary numeric conversion, does the same as Number(...)
-console.log(+true) // 1
-console.log(+"") // 0
-console.log(+{}) // NaN
-console.log(+"123") // 123
+console.log(+true); // 1
+console.log(+""); // 0
+console.log(+{}); // NaN
+console.log(+"123"); // 123
 
 let apples = "2";
 let oranges = "3";
@@ -79,15 +79,15 @@ d = e = f = 2 + 2;
 // everything equals 4
 
 {
-  const a = 3
-  console.log(a)
+  const a = 3;
+  console.log(a);
 }
 // sometimes you can just make your own block
 
 {
-  let counter = 1
-  let a = ++counter
-  console.log(a) // 2, since ++ before is prefix and returns the new val, as opposed to postfix which returns the old val
+  let counter = 1;
+  let a = ++counter;
+  console.log(a); // 2, since ++ before is prefix and returns the new val, as opposed to postfix which returns the old val
 }
 
 {
@@ -99,7 +99,7 @@ d = e = f = 2 + 2;
   // without them: a = 1 + 2, 3 + 4 evaluates + first, summing the numbers into a = 3, 7, then the assignment operator = assigns a = 3, and the rest is ignored. It’s like (a = 1 + 2), 3 + 4
 }
 
-console.log(null > 0);  // (1) false
+console.log(null > 0); // (1) false
 console.log(null == 0); // (2) false
 console.log(null >= 0); // (3) true
 // ^ comparators convert to numbers, so null becomes 0. == leaves null and undefined as they are. == does convert other data types to numbers though.
--- non-react/random.js
+++ non-react/random.js
@@ -1,29 +1,28 @@
-let i = 3
+let i = 3;
 do {
-  i--
-  console.log('doing:', i)
-}
-while (i > 0)
+  i--;
+  console.log("doing:", i);
+} while (i > 0);
 
 // parts of loops can be omitted: for(;;) actually works
 
-console.log(false == 0, 'should be true because of coercion?')
+console.log(false == 0, "should be true because of coercion?");
 
 // transpilers convert code to older JavaScript code that older JavaScript engines can read
 // polyfills fills in when there is a lack of implementation for things like newer functions (like Math.trunc)
 
-let user = new Object()
-user["with space"] = "it worked"
+let user = new Object();
+user["with space"] = "it worked";
 
-console.log(user, 'wow you can make an object this way {} and use space')
+console.log(user, "wow you can make an object this way {} and use space");
 
-let variable = "variable"
+let variable = "variable";
 
 let bag = {
-  [variable]: 5
-}
+  [variable]: 5,
+};
 
-console.log(bag, bag[variable])
+console.log(bag, bag[variable]);
 
 // there are no limitations on property names. you can use return, like obj.return.
 
@@ -32,11 +31,36 @@ console.log(bag, bag[variable])
 
 const sortedObj = {
   3: "",
-  "2": "",
+  2: "",
   "don't sort": "",
-  1: ""
-}
+  1: "",
+};
 
 for (const key in sortedObj) {
-  console.log(key, 'key')
+  console.log(key, "key");
 }
+
+const obj1 = {
+  name: "Bert",
+  color: "blue",
+  obj: {},
+};
+
+const obj2 = {
+  name: "Jobert",
+  shirtSize: "M",
+};
+
+console.log(Object.assign(obj1, obj2), "Object.assign...");
+
+const clone = Object.assign({}, obj1);
+console.log(clone.obj == obj1.obj, "is clone equal?");
+
+const structuredCloneObj = structuredClone(obj1); // deeper cloner
+
+console.log(structuredCloneObj.obj == obj1.obj, "is clone equal?");
+
+// however structedClone does not clone functions
+
+obj1.reflect = obj1;
+console.log(obj1 == obj1.reflect, "circular?");
